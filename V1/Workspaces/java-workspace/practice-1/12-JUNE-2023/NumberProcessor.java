2dfe05a00c54417bfb34ef0283d59227686d3a2f
tech-prasad <techie.prasadl@gmail.com>
2023-06-13 11:24:36 +0530

    MethodOverloading

--- V1/Workspaces/java-workspace/practice-1/12-JUNE-2023/NumberProcessor.java
+++ V1/Workspaces/java-workspace/practice-1/12-JUNE-2023/NumberProcessor.java
@@ -1,23 +1,26 @@
 public class NumberProcessor {
-	
 	public void addAndPrint(int data){
-		
 		int addedValue = data + 2;
 
 		System.out.println(addedValue);
 	}
-	
-	public void addAndPrint(int data, int data2){
-		
-		int addedValue = data + 78;
+	public void addAndPrint(int x, int y){
+		int addedValue = x + y + 78;
 
 		System.out.println(addedValue);
 	}
-	
+	public void addAndPrint(int data, double y){
+		double addedValue = data + y + 78;
+
+		System.out.println(addedValue);
+	}
 	public static void main(String[] array){
-		
 		NumberProcessor ref = new NumberProcessor();
-				
 		ref.addAndPrint(44);
+		ref.addAndPrint(44, 66);
+		ref.addAndPrint(44, 66.8);
 	}
 }
+
+
+
