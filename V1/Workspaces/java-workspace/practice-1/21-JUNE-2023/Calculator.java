153363a69ab79187efe0f0440084009de2dca226
tech-prasad <techie.prasadl@gmail.com>
2023-06-21 11:39:07 +0530

    Compilation and Running

---
+++ V1/Workspaces/java-workspace/practice-1/21-JUNE-2023/Calculator.java
@@ -0,0 +1,24 @@
+
+public class Calculator extends Object {
+	public void add(){
+		System.out.println(2+5);
+		
+		int hashCode1 = super.hashCode();
+		
+		System.out.println("Hashcode1: "+hashCode1);
+		
+		int hashCode2 = hashCode();
+		
+		System.out.println("Hashcode2: "+hashCode2);
+	}
+	public static void main1(String[] arr){
+		Calculator c = new Calculator();
+		c.add();
+	}
+	
+	public static void main(String[] arr){
+		Calculator c = new Calculator();
+		c.add();
+	}
+	
+}
