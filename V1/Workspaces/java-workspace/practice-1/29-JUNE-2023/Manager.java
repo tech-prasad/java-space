c5e22e63c96f99f08cb8a56bd0b5e76eb9321dfb
tech-prasad <techie.prasadl@gmail.com>
2023-06-29 11:24:16 +0530

    package explanation

---
+++ V1/Workspaces/java-workspace/practice-1/29-JUNE-2023/Manager.java
@@ -0,0 +1,17 @@
+package com.icicibank.netbank.staff;
+
+import com.icicibank.netbank.common.Address;
+
+public class Manager {
+	
+	int managerId;
+	String name;
+	String email;
+	String phoneNumber;
+	Address address;
+	
+	public static void main(String[] arr){
+		System.out.println("Manager testing");
+	}
+	
+}
