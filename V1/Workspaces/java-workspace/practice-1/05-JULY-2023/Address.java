280004be2fc498bbe5cc3f12dfe1737316b3f992
tech-prasad <techie.prasadl@gmail.com>
2023-07-05 11:39:02 +0530

    access modifiers and data types

---
+++ V1/Workspaces/java-workspace/practice-1/05-JULY-2023/Address.java
@@ -0,0 +1,25 @@
+package com.icicibank.netbank.common;
+
+public class Address {
+	public String houseNumber;
+	public String city;
+	public String state;
+	public final String country;
+		
+	public Address(){
+		this.country="test";
+	}
+	
+	public Address(String houseNumber, String city, String state, String country){
+		this.houseNumber=houseNumber;
+		this.city=city;
+		this.state=state;
+		this.country=country;		
+	}
+	public void printAddress(){
+		System.out.printf("%S, %S, %S, %S\n", houseNumber,city,state,country);
+	}
+	public final void test(){
+		System.out.println("test from address");
+	}
+}
---
+++ V1/Workspaces/java-workspace/practice-2/DataTypes/src/com/number/test/Numbers.java
@@ -0,0 +1,9 @@
+package com.number.test;
+
+public class Numbers {
+
+	public byte byteVariable = 126;
+	public short shortVariable = -32765;
+	public int intVariable = 34567785;
+	
+}
